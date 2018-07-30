d8780a17c2aee54f31d177348cea7d9aff5ee1c9
denso <takuya_iwasa@denso.co.jp>
2018-07-30 18:34:22 +0900

    first commit

---
+++ facer.py
@@ -0,0 +1,46 @@
+import cv2
+import sys
+
+class Facer:
+
+    def __init__(self,cascade):
+#        self.classifier = cv2.CascadeClassifier('haarcascade/'+ str(cascade))
+# CascadeClassifier direct input 2018/5/1
+        self.classifier = cv2.CascadeClassifier("./haarcascade_frontalface_alt.xml")
+
+    def draw_rect(self, encode, color=tuple([0, 0, 255]), thickness=5):
+        img_raw = self.decode_img_for_cv(encode)
+        gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
+        face = self.classifier.detectMultiScale(gray, 1.11, 3)
+        if len(face) > 0:
+            for lect in face:
+                x, y, w, h = lect
+                cv2.rectangle(img_raw, (x, y), (x+w, y+h),color=color,thickness=thickness)
+        else:
+            sys.stdout.write('(no face) ')
+        return img_raw
+
+    def cut_face(self, encode):
+        img_raw = self.decode_img_for_cv(encode)
+        try:
+            gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
+            face = self.classifier.detectMultiScale(gray, 1.11, 3)
+            if len(face) > 0:
+                x, y, w, h = face[0]
+                cropd = img_raw[y:y+h, x:x+w]
+                return cropd
+            else:
+                sys.stdout.write('(no face) ')
+                return img_raw
+        except:
+            sys.stdout.write('(CV error) ')
+            return img_raw
+
+    def decode_img_for_cv(self, encode):
+        return cv2.imdecode(encode, -1)
+
+    def save_img(self, img, file):
+        try:
+            cv2.imwrite(file, img)
+        except:
+            sys.stdout.write('(Error in image saving) ')
---
+++ gather.py
@@ -0,0 +1,157 @@
+from urllib.request import urlopen, Request
+from urllib.parse import quote
+from mimetypes import guess_extension
+import os
+import sys
+import numpy as np
+from bs4 import BeautifulSoup
+from time import time, sleep
+from facer import Facer
+
+MY_UA = ''
+
+class Fetcher:
+	'''
+	受け取ったurlを指定されたuaでリクエストし、html、MIMEタイプとエンコード（Content-Type）を貰う
+	'''
+	def __init__(self, ua=''):
+		self.ua = ua
+
+	def fetch(self, url):
+		req = Request(url, headers={'User-Agent': self.ua})
+		try:
+			with urlopen(req, timeout=3) as p:
+				content = p.read()
+				mime = p.getheader('Content-Type')
+		except:
+			sys.stderr.write('Error in fetching :'+ format(url) + "\n")
+
+			return None, None
+
+		return content, mime
+
+
+def html_scraping(url):
+	img_link_elem = []
+	byte_content, mime = fetcher.fetch(url)
+	soup = BeautifulSoup(byte_content.decode('UTF-8'), 'html.parser')
+	elem = soup.find_all('img')
+	img_link_elem.extend(elem)
+	return img_link_elem
+
+
+def url_search(word, n):
+	'''
+	ヤフーの画像検索を使って画像収集
+	:param word: 検索キーワード
+	:param n: 枚数
+	:return: 画像のURL群　これをpcのフォルダにDLして終わり
+	'''
+	code = '&ei=UTF-8'
+	#URLの受け皿
+	img_link_elem = []
+
+
+	if n > 60:
+		iter_num = int(n / 60)
+		rem = n % 60
+		print(iter_num)
+
+	if not 'iter_num' in locals():
+		url = ('https://search.yahoo.co.jp/image/search?n={}&p={}'+code).format(n, quote(word))
+		# byte_content, mime = fetcher.fetch(url)
+		# soup = BeautifulSoup(byte_content.decode('UTF-8'), 'html.parser')
+		# # elem = soup.find_all('a', attrs={'target': 'imagewin'})
+		# elem = soup.find_all('img')
+		# img_link_elem.extend(elem)
+		img_link_elem = html_scraping(url)
+	else:
+		for i in range(2,iter_num+2):
+			url = ('https://search.yahoo.co.jp/image/search?n=60&p={}{}' + code).format(quote(word), i)
+			# byte_content, mime = fetcher.fetch(url)
+			# soup = BeautifulSoup(byte_content.decode('UTF-8'), 'html.parser')
+			# elem = soup.find_all('a', attrs={'target': 'imagewin'})
+			# img_link_elem.extend(elem)
+			img_link_elem = html_scraping(url)
+		if rem != 0:
+			url = ('https://search.yahoo.co.jp/image/search?n={}&p={}').format(rem, quote(word))
+			# byte_content, mime = fetcher.fetch(url)
+			# soup = BeautifulSoup(byte_content.decode('UTF-8'), 'html.parser')
+			# add_elem = soup.find_all('a', attrs={'target': 'imagewin'})
+			# img_link_elem.extend(add_elem)
+			img_link_elem = html_scraping(url)
+
+	# img_urls = [e.get('href') for e in img_link_elem if e.get('href').startswith('http')]
+	img_urls = [e.get('src') for e in img_link_elem]
+
+	#重複を取り除くテク
+	img_urls = list(set(img_urls))
+	return img_urls
+
+def image_collector_in_url(urls,fname,command):
+	'''
+	取得したURL群をfetchし画像ダウンロード
+	:param urls:  画像の存在するURL
+	:param fname: 作成されるフォルダの名前
+	:param command: 顔の編集モード指定番号
+	:return: ダウンロードに成功した数
+	'''
+
+	dir = 'img/'+str(fname)
+	scount = 0
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+
+		for i , url in enumerate(urls):
+			sleep(0.1)
+			img, mime = fetcher.fetch(url)
+			if not mime or not img:
+				continue
+			try:
+				ext = guess_extension(mime.split(';')[0])
+				if ext in ('.jpeg', '.jpg', '.png', '.jpe'):
+					ext = '.png'
+				elif not ext:
+					continue
+			except:
+				print('Error in converting extension')
+				continue
+			file = os.path.join(dir, str(i) + ext)
+			command_dict = {
+				0: (lambda encode: facer.draw_rect(encode)),
+				1: (lambda encode: facer.cut_face(encode))
+			}
+			print('file:' + file)
+			# print(img)
+
+			#顔編集実行
+			if command in command_dict.keys():
+				encode = np.asarray(bytearray(img), dtype=np.uint8)
+				img = command_dict[command](encode)
+				facer.save_img(img, file)
+				print('ダウンロード成功 URL:'+str(url))
+				scount += 1
+			else:
+				with open(file, mode='wb') as f:
+					f.write(img)
+				print('ダウンロード成功 URL:'+str(url))
+				scount += 1
+
+		return scount
+
+
+if __name__ == '__main__':
+
+	fetcher = Fetcher(MY_UA)
+	facer = Facer('haarcascade_frontalface_alt.xml')
+
+	print('欲しい画像のキーワードを入力してください')
+	keyword = input('>> ')
+	print('枚数を入力してください')
+	get_num = int(input('>> '))
+	print('顔の編集モードを指定してください　輪郭描画:0　顔のみ切り出し:1　編集なし:2')
+	command = int(input('>> '))
+	image_urls = url_search(keyword, get_num)
+	print(str(len(image_urls))+'枚の画像URLの取得に成功しました')
+	download_count = image_collector_in_url(image_urls, keyword, command)
+	print(str(download_count)+'件の画像の収集に成功しました')
